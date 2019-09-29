74064ea1fc3841887df4af83ebf54b735f044fc1
sabina929 <sabina.abbasova2909@gmail.com>
2019-09-29 20:31:04 +0400

    Single Room page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Services from "../components/Services";
 import FeaturedRooms from "../components/FeaturedRooms";
 
+
 function Home() {
   return (
     <React.Fragment>
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,101 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import defaultBcg from "../images/room-1.jpeg";
+// import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
+import StyledHero from "../components/StyledHero"
+
+
+
 
 class SingleRoom extends Component {
+
+  constructor(props) {
+    super(props)
+
+    // console.log(this.props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg
+    }
+
+  }
+
+
+  static contextType = RoomContext;
+  // componentDidMount() {}
+
   render() {
+
+    const {getRoom} = this.context;
+
+    const room = getRoom(this.state.slug);
+
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No such room could be found...</h3>
+          <Link to="/rooms" className="btn-primary">Back To Rooms</Link>
+        </div>
+      )
+    }
+
+    const {name, description, capacity, size, price, extras, breakfast, pets, images} = room;
+    // console.log(room);
+
+    const [mainImg, ...defaultImg] = images;
+    // console.log(defaultImg);
+
     return (
-      <div>
-               Single Room 
-            </div>
+      <React.Fragment>
+      <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} Room`}>
+            <Link to="/rooms" className="btn-primary">Back To Rooms</Link>
+          </Banner>              
+      </StyledHero>
+
+<section className="single-room">
+  <div className="single-room-images">
+
+      {defaultImg.map((image, i) => {
+        return (<img key={i} src={image} alt={name}/>)
+      })}
+
+  </div>
+
+   <div className="single-room-info">
+
+     <article className="desc">
+       <h3>Details</h3>
+       <p>{description}</p>
+     </article>
+     <article className="info">
+       <h3>Info</h3>
+       <h6>Price: ${price}</h6>
+       <h6>Size: {size} SQFT</h6>
+       <h6>Max Capacity: {capacity > 1 ? `${capacity} people` : `${capacity}person`}</h6>
+       <h6>{pets ? 'Pets Allowed' : 'No Pets Allowed'}</h6>
+       <h6>{breakfast && 'Free Breakfast Included'}</h6>
+     </article>
+
+   </div>
+</section>
+
+<section className="room-extras">
+  <h6>Extras</h6>
+  <ul className="extras">
+    {
+      extras.map((extra, i) => {
+        return (
+          <li key={i}>- {extra}</li>
+        )
+      })
+      }
+  </ul>
+</section>
+
+      </React.Fragment>
     )
   }
 }
